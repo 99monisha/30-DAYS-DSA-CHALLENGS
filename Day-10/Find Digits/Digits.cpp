f8df39f77de81ca8417f848bda58c1a3ca94bf8b
Monisha Dolai <monishadolai1998@gmail.com>
2022-06-20 13:05:02 +0530

    day-10

--- Day-10/Find Digits/Digits.cpp
+++ Day-10/Find Digits/Digits.cpp
@@ -1,6 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int t,i,digit,p=0;
+    long long int n,x;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        cin>>n;
+        x=n;
+        while(n!=0)
+        {
+            
+            digit=n%10;
+            if(digit!=0)
+            {
+            if(x%digit==0)
+            p++;
+            }
+            n/=10;
+        }
+        cout<<p<<endl;
+        p=0;
+    }
     return 0;
 }
