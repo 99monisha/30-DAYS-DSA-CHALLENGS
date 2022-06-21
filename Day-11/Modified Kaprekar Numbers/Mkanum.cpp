5aaf4bd9179dbff1ee8c051c4289c37efdc32220
Monisha Dolai <monishadolai1998@gmail.com>
2022-06-21 11:56:37 +0530

    Day-11

---
+++ Day-11/Modified Kaprekar Numbers/Mkanum.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    long long p,q,i;
+    cin>>p;
+    cin>>q;
+    vector<long long> res;
+    for(i=p; i<=q; ++i){
+        long long sq = i*i;
+        string s = to_string(sq);
+        int d = s.size()/2;
+        if(d == 0){
+            if(i == sq){
+                res.push_back(i);
+            }
+            continue;
+        }
+        if(stoll(s.substr(0,d))+stoll(s.substr(d,s.size()-d)) == i){
+            res.push_back(i);
+        }
+           }
+    if(res.size()>0){
+           for(int i=0; i<res.size(); ++i){
+               cout<<res[i]<<" ";
+           }
+        cout<<endl;}
+    else{
+        cout<<"INVALID RANGE"<<endl;
+    }
+           return 0;
+}
