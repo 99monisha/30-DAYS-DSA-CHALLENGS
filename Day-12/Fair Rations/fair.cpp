6e404e5e2110e7da0edcc0a0899e5184f9b0ae47
Monisha Dolai <monishadolai1998@gmail.com>
2022-06-22 19:33:01 +0530

    Day-12

--- Day-12/Fair Rations/fair.cpp
+++ Day-12/Fair Rations/fair.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 int main()
 {
-    
+  int n,i,bread;cin>>n;
+  int a[n];
+  for(i=0;i<n;i++)
+  cin>>a[i];
+  for(i=0;i<n-1;i++)
+  {
+    if(a[i]%2!=0)
+    {
+        a[i]=a[i]+1;
+        a[i+1]=a[i+1]+1;
+        bread+=2;
+    }
+  }  
 
-
+if(a[n-1]%2!=0)
+cout<<"NO";
+else{
+    cout<<bread;
+}
     return 0;
 }
