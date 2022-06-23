8f54749ae94f8309a0caeb64dca00d0035cc7519
Monisha Dolai <monishadolai1998@gmail.com>
2022-06-23 21:19:24 +0530

    .

--- Day-13/Mars Exploration/mars.cpp
+++ Day-13/Mars Exploration/mars.cpp
@@ -2,13 +2,12 @@
 using namespace std;
 int main()
 {  
-   int ans=0,i,j,n;
-   char s[100],a[3];
-   cin>>s;
-
-   n=strlen(s);
-   for(i=0;i<n;i=i+3)
-   {
+    int ans=0,i,j,n;
+    char s[100],a[3];
+    cin>>s;
+    n=strlen(s);
+    for(i=0;i<n;i=i+3)
+    {
     if(s[i]!='S')
     ans++;
     if(s[i+1]!='O')
