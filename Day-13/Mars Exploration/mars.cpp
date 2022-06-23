07983391f8f07b0e40b0b0f55b3e93fe24d555ef
Monisha Dolai <monishadolai1998@gmail.com>
2022-06-23 21:13:12 +0530

    Day-13

---
+++ Day-13/Mars Exploration/mars.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{  
+   int ans=0,i,j,n;
+   char s[100],a[3];
+   cin>>s;
+
+   n=strlen(s);
+   for(i=0;i<n;i=i+3)
+   {
+    if(s[i]!='S')
+    ans++;
+    if(s[i+1]!='O')
+    ans++;
+    if(s[i+2]!='S')
+    ans++;
+   }
+   cout<<ans;
+
+    return 0;
+}
